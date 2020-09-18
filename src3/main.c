8949419c8e5028abcba6bece2c504072440501db
Altair200333 <petrenko-m-p@mail.ru>
2020-09-18 09:47:22 +0700

    commit

--- src3/main.c
+++ src3/main.c
@@ -1,8 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
+#define path "test.txt"
+
+int try()
+{
+	printf("The real user ID is %d\n", getuid());
+    printf("The effective user ID is %d\n", geteuid());
+
+	FILE* f = fopen(path, "r");
+	if(f == NULL)
+	{
+		perror("File pointer = NULL\n");	
+		return -1;
+	}
+	else
+	{
+		printf("file opened successfully\n");
+		fclose(f);
+		return 0;
+	}
+}
 int main()
 {
-  return 0;
+	int res = try();
+	if (res == 0)
+	{
+		setuid(getuid());
+		try();
+	}
+	
+	return 0;
 }
 
